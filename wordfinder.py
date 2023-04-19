e1cbaccd5556092cb0b1602d7def1f7c9bef31b9
Crynowecer0 <crynowecer11@gmail.com>
2023-04-18 22:03:39 -0400

    progress on special wordfinder

--- wordfinder.py
+++ wordfinder.py
@@ -4,15 +4,13 @@ class WordFinder:
     """Word Finder: finds random words from a dictionary."""
     def __init__(self,file_path):
         """creates a list of words from a text file"""
-        WordFinder.open_and_make_read_list(file_path)
+        print('i am inside my parent')
+        file = open(file_path)
+        self.list_of_words = self.read_file(file)
         print(f"{len(self.list_of_words)} words read")
 
-    @classmethod
-    def open_and_make_read_list(self, file_path):
-        self.file = open(file_path)
-        self.list_of_words = [line.replace("\n","") for line in self.file]
-        self.file.close()
-        return self.list_of_words
+    def read_file(self, file):
+        return [line.strip() for line in file]
 
     def __repr__(self):
         return f"<WordFinder list_of_words contains {len(self.list_of_words)} words"
@@ -24,13 +22,12 @@ class WordFinder:
 
 class SpecialWordFinder(WordFinder):
 
-    def __init__(self, file_path):
-        super().__init__(file_path)
-        SpecialWordFinder.remove_sharp_and_blank(self)
+    # def __init__(self, file_path):
+    #     # super().__init__(file_path)
+    #     # SpecialWordFinder.remove_sharp_and_blank(self)
 
-    def remove_sharp_and_blank(self):
-        self.new_list_of_words = [line for line in self.list_of_words if line.startswith("#")==False or line.isspace()==False]
-        return self.new_list_of_words
+    def read_file(self, file):
+        return [line for line in super().read_file(file) if not line.startswith("#") and line != '']
 
 
 
