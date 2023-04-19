496491070b5772cb1f1193e3121a9b1aeaba8e6e
MKim <mrn.kim07@gmail.com>
2023-04-18 17:12:26 -0700

    work in progress on SpecialWordFinder

--- wordfinder.py
+++ wordfinder.py
@@ -4,10 +4,15 @@ class WordFinder:
     """Word Finder: finds random words from a dictionary."""
     def __init__(self,file_path):
         """creates a list of words from a text file"""
+        WordFinder.open_and_make_read_list(file_path)
+        print(f"{len(self.list_of_words)} words read")
+
+    @classmethod
+    def open_and_make_read_list(self, file_path):
         self.file = open(file_path)
         self.list_of_words = [line.replace("\n","") for line in self.file]
         self.file.close()
-        print(f"{len(self.list_of_words)} words read")
+        return self.list_of_words
 
     def __repr__(self):
         return f"<WordFinder list_of_words contains {len(self.list_of_words)} words"
@@ -17,12 +22,15 @@ class WordFinder:
         return choice(self.list_of_words)
 
 
+class SpecialWordFinder(WordFinder):
+
+    def __init__(self, file_path):
+        super().__init__(file_path)
+        SpecialWordFinder.remove_sharp_and_blank(self)
+
+    def remove_sharp_and_blank(self):
+        self.new_list_of_words = [line for line in self.list_of_words if line.startswith("#")==False or line.isspace()==False]
+        return self.new_list_of_words
 
 
 
-#instantiated with path to file of words
-# prints out the number of the words?
-# saves list of words behind the scenes
-
-#random method
-# grab random word from saved list and return it
